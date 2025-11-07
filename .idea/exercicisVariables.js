67d9edf631b9563818051f5bdd13e17c1beb6af9
kituss44 <marc.romaguera@institutvidreres.cat>
2025-11-07 11:57:04 +0100

    exercici_4

--- .idea/exercicisVariables.js
+++ .idea/exercicisVariables.js
@@ -1,11 +1,11 @@
-//Exercici 1
-//console.log("Hem dic Marc Romaguera Fernandez i tinc 18 anys")
+/*Exercici 1
+console.log("Hem dic Marc Romaguera Fernandez i tinc 18 anys")*/
 
-//Exercici 2
-//var primer = true
-//var segon = 'wasd'
-//var tercer = 44
-//console.log(primer, segon, tercer)
+/*Exercici 2
+var primer = true
+var segon = 'wasd'
+var tercer = 44
+console.log(primer, segon, tercer)*/
 
 /*Exercici 3
 console.log(dada_1)
@@ -14,3 +14,9 @@ var dada_1 = 'mec'
 let dada_2 = 'mec'
 
 Amb var surt indefinida, i amb let surt error.*/
+
+/*Exercici 4
+var valor = 0
+console.log(valor)
+valor = 'wasd'
+console.log(valor)*/
