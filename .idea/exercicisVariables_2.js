c0145d5a6142299d9b6e7bfb2a830d9774ded307
kituss44 <marc.romaguera@institutvidreres.cat>
2025-11-07 12:12:09 +0100

    exercici_6

---
+++ .idea/exercicisVariables_2.js
@@ -0,0 +1,7 @@
+/*Exercici 6
+var num_1 = 1234
+var num_2 = 5678
+console.log('Suma: ' + (num_1+num_2))
+console.log('Resta: '+ (num_1-num_2))
+console.log('Multiplicación: ' + (num_1*num_2))
+console.log('División: '+ (num_1/num_2))*/
