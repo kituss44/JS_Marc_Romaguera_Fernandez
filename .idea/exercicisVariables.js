75b509e6822d8b81ea0d611687e908953a0a0ce8
kituss44 <marc.romaguera@institutvidreres.cat>
2025-11-07 11:19:15 +0100

    Exercici_2

--- .idea/exercicisVariables.js
+++ .idea/exercicisVariables.js
@@ -1,1 +1,8 @@
-console.log("Hem dic Marc Romaguera Fernandez i tinc 18 anys")
+//Exercici 1
+//console.log("Hem dic Marc Romaguera Fernandez i tinc 18 anys")
+
+//Exercici 2
+//var primer = true
+//var segon = 'wasd'
+//var tercer = 44
+//console.log(primer, segon, tercer)
