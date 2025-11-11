5a3abbccc4814dae39c008f2d46903cebe2c0c5a
kituss44 <marc.romaguera@institutvidreres.cat>
2025-11-11 13:07:51 +0100

    exercici_7,8

--- .idea/exercicisVariables_2.js
+++ .idea/exercicisVariables_2.js
@@ -5,3 +5,16 @@ console.log('Suma: ' + (num_1+num_2))
 console.log('Resta: '+ (num_1-num_2))
 console.log('Multiplicación: ' + (num_1*num_2))
 console.log('División: '+ (num_1/num_2))*/
+
+/* Exercici 7
+
+var catet = 4
+console.log("la hipotenusa es: ", (Math.sqrt(catet**2 + catet**2)))*/
+
+/*Exercici 8
+
+var num = 10
+var str = "hola"
+console.log(num/str)*/
+
+//Exercici 9
