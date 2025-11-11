eb68e918175403229cf462d2f47ce9ee31cbcce9
kituss44 <marc.romaguera@institutvidreres.cat>
2025-11-11 13:48:07 +0100

    exercici9,10,11,12,13,14

--- .idea/exercicisVariables_2.js
+++ .idea/exercicisVariables_2.js
@@ -17,4 +17,54 @@ var num = 10
 var str = "hola"
 console.log(num/str)*/
 
-//Exercici 9
+/*Exercici 9
+
+var nom = "Marc Romaguera Fernandez"
+var part = nom.split(" ")
+var cog_1 = part[1]
+console.log(cog_1)
+console.log(cog_1.length)*/
+
+/*Exercici 10
+
+var nom = 'Marc Romaguera Fernandez'
+var nom_sense_6 = nom.slice(0,5)+nom.slice(6)
+console.log(nom_sense_6)*/
+
+/*Exercici 11
+
+var undef = undefined
+var nul = null
+console.log(nul+undef)*/
+
+/*Exercici 12
+
+var bool = true
+var strng = bool.toString()
+console.log(strng)*/
+
+/*Exercici 13
+
+var text1 = "hola"
+var text2 = ""
+var bool1 = Boolean(text1)
+var bool2 = Boolean(text2)
+console.log(bool1)
+console.log(bool2)
+if (text1) {
+    console.log("text1 és true");
+}
+if (!text2) {
+    console.log("text2 és false");
+}*/
+
+/*Exercici 14
+
+let a = true
+let b = false
+let num1 = Number(a)
+let num2 = Number(b)
+console.log(num1)
+console.log(num2)
+console.log(a * 10)
+console.log(b + 5)*/
