885ec2496e10526fc700a9b5baa88b1ef41a15a2
kituss44 <marc.romaguera@institutvidreres.cat>
2025-11-14 11:39:15 +0100

    exercicis_funcions_1-6

---
+++ .idea/funcions/exercicisFuncions.js
@@ -0,0 +1,50 @@
+/*Exercici 1
+
+function nom() {
+    console.log('Marc')
+    console.log('Romaguera')
+    console.log('Fernandez')
+}
+nom()*/
+
+/*Exercici 2
+
+function valor(valor) {
+    console.log(typeof valor)
+}
+valor(613)
+valor('patata')
+valor(true)*/
+
+/*Exercici 3
+
+function data() {
+    return new Date()
+}
+console.log(data())*/
+
+/*Exercici 4
+
+function funcio() {
+    var patata = 10
+}
+console.log(patata)*/
+
+/*Exercici 5
+
+function comptaVocals(text) {
+    var vocals = "aeiouAEIOU";
+    var comptador = 0;
+
+    for (let i = 0; i < text.length; i++) {
+        if (vocals.includes(text[i])) {
+            comptador++;
+        }
+    }
+
+    return comptador;
+}
+console.log(comptaVocals('patata'))*/
+
+//Exercici 6
+
